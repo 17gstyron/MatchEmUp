fdee4b90fa48eea1e60a33a44c6e0a40f8ab3f7b
jessicatorres-debug <jes.tores2277.jt@gmail.com>
2020-07-30 17:11:23 -0500

    Add the game title and postion while resizing the menu container component

--- app/src/components/Menu.jsx
+++ app/src/components/Menu.jsx
@@ -11,7 +11,7 @@ const useStyles = makeStyles((theme) => ({
     position: 'absolute',
     bottom: 5,
     right: 5,
-    width: 178,
+    width: 160,
     height: 68,
     borderRadius: '30%',
 
@@ -26,6 +26,17 @@ const useStyles = makeStyles((theme) => ({
     height: theme.spacing(8),
     // margin: 'auto',
   },
+  container: {
+    width: '50rem',
+    height: '20rem',
+    marginTop: 100,
+    boxShadow: '0 0 1rem 0 rgba(0, 0, 0, .2)',   
+    borderRadius: 5,
+    borderColor: 'black',
+    backgroundColor: 'rgba(255, 255, 255, .15)',
+    
+    backdropFilter: 'blur(5px)',
+  },
 }));
 
 const Menu = () => {
@@ -33,14 +44,16 @@ const Menu = () => {
 
   return (
     <div className="Menu">
-     
-        <Box className={classes.credits}>
-          <Grid container justify="space-around">
-            <img src={logo} className={classes.large}></img>
-            <h7 className={classes.ackn}>creations</h7>
-          </Grid>
-        </Box>
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+      <h4 className="title">Match Em Up!</h4>
+      <div className={classes.container}></div>
+
+      <Box className={classes.credits}>
+        <Grid container justify="space-around">
+          <img src={logo} className={classes.large}></img>
+          <h7 className={classes.ackn}>creation</h7>
+        </Grid>
+      </Box>
+      {/* <img src={logo} className="App-logo" alt="logo" /> */}
     </div>
   );
 }
